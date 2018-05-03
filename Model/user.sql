5f132e5d91fb751cfbf56fef8d534e96de425cf2
HugoDem <hugo.demarcq@gmail.com>
2018-05-03 14:03:10 +0200

    base de données user

--- Model/user.sql
+++ Model/user.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:3306
--- Généré le :  jeu. 29 mars 2018 à 14:19
+-- Généré le :  jeu. 03 mai 2018 à 12:01
 -- Version du serveur :  5.6.34-log
 -- Version de PHP :  7.1.7
 
@@ -33,7 +33,7 @@ CREATE TABLE `user` (
   `nom` varchar(50) NOT NULL,
   `prenom` varchar(50) NOT NULL,
   `mail` varchar(50) NOT NULL,
-  `motdepasse` varchar(20) NOT NULL,
+  `motdepasse` varchar(300) NOT NULL,
   `numero` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -42,10 +42,16 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `nom`, `prenom`, `mail`, `motdepasse`, `numero`) VALUES
-(1, 'parisot', 'elodie', 'elodie.parisot@live.fr', 'laplusbelle', 635163984),
-(2, 'collot', 'adrien', 'adrien.collot@isep.fr', 'leplusbeau', 695712105),
-(3, 'demarcq', 'hugo', 'hugo.demarcq@isep.fr', 'leplushchiant', 656468374),
-(4, 'ly', 'felix', 'felix.ly@isep.fr', 'leplusdrole', 667483945);
+(13, 'Hugo', 'Demarcq', 'hugo@isep.fr', '$2y$10$z..rgeM37z/Gbqmryvn8q.h4TiE3NK1o25fhxSV6G9MjuAFWubP52', 987654321),
+(14, 'Adrien', 'Collot', 'adrien@isep.fr', '$2y$10$FYJ8LT.XvtS7REHXa50lEeJeUGrE8n.OXZkHbKT3y0YEYWaTsItdC', 987654321),
+(15, 'Elodie', 'Parisot', 'elodie@isep.fr', '$2y$10$pQpUgrRVR1OQlIodgWtu.OcYx7FuLh2mMmtFzQZ2Ta8GEVArHt6kS', 987654321),
+(16, 'Felix', 'Ly', 'felix@isep.fr', '$2y$10$BH1liAwjjOPR1/AOoF.VAuJHLCRAa9SsUYgX5x8rwvjlyy5E5x7R.', 987654321),
+(17, 'Michel', 'Galabru', 'Michga@mail.fr', '$2y$10$jG4XdTfzslNzjyfZF3eXauB7Cge5K7R7biqqdzLXSf8sgli9W8KGO', 987654321),
+(19, 'papa', 'outai', 'paps@mail.com', '$2y$10$vfobb.aGAgbBC3/o.Q6Q1.IlsEivZbe96pGugJS6L36asKYlGGOjW', 987654321),
+(20, 'Bisou', 'Nours', 'zoubi@mail.fr', '$2y$10$0ActVtbRqeVOJ9xoGgeseeEGqBFjofbfuLOtTK5QDvrOURWGXad4q', 987654321),
+(21, 'Paul', 'Pogba', 'popol@mail.fr', '$2y$10$MGkGMnFLXCsxtRVpDo8N0uie.5/D5aeiagNTiOW4iuU3ozKBNLNhS', 987654321),
+(22, 'Sylvain', 'Duriff', 'sylv@mail.fr', '$2y$10$7r/DjN.g3YiUR6BWqtZrg.s1zXkEqG6fT5HeYb10i.sGKgud2Or8.', 987654321),
+(23, 'Peppa', 'Pig', 'peppa@mail.fr', '$2y$10$yh17vVgmlCxJkYk5mFbfyez/1RNC/XnmegQK4E0c5MeXFkHOE8xg.', 987654321);
 
 --
 -- Index pour les tables déchargées
@@ -65,7 +71,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT pour la table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;COMMIT;
+  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
